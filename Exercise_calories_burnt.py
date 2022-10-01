e0b11f8d5468f00322718e23bd991df851a4edf8
Sujay Mishra <95263133+SujayMishra95@users.noreply.github.com>
2022-10-01 14:14:51 +0200

    Add files via upload

---
+++ Exercise_calories_burnt.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+
+import pickle
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+
+# loading the saved models
+
+#exercise_model = pickle.load(open('C:/Users/Sujay/Desktop/Exercise Calories/exercise_cals_sk.sav', 'rb'))
+exercise_model = pickle.load(open('exercise_cals_sk.sav', 'rb'))
+    # page title
+st.title('Calories Burnt Prediction using Machine Learning')
+    
+Gender = st.text_input('Gender (Male:0, Female:1)')
+Age = st.text_input('Age (years)')
+Height = st.text_input('Height (cm)')
+Weight = st.text_input('Weight (kg)')
+Duration = st.text_input('Exercise Duration (mins)')
+Heart_Rate = st.text_input('Heart Rate (bps)')
+Body_Temp = st.text_input('Body Temperature (Degrees Celcius)')
+ 
+     
+     
+    # code for Prediction
+cals_burnt_pred = ''
+    
+    # creating a button for Prediction
+    
+if st.button('Number of Calories burnt result'):
+      cals_burnt = exercise_model.predict([[Gender, Age, Height, Weight, Duration, Heart_Rate, Body_Temp]])                          
+     
+      cals_burnt_pred = cals_burnt
+st.success(cals_burnt_pred)
+
+    
